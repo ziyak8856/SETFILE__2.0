0625a385f8c489c5f2535f1da5580ff9e43e9fe5
ziya ur raheman <ziya.khan@iiitb.ac.in>
2025-06-01 00:36:20 +0530

    Created project  with all related tables

--- DATABASE/setfile.sql
+++ DATABASE/setfile.sql
@@ -16,33 +16,37 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `jn16_prs_cphy_12`
+-- Table structure for table `setfile`
 --
 
-DROP TABLE IF EXISTS `jn16_prs_cphy_12`;
+DROP TABLE IF EXISTS `setfile`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `jn16_prs_cphy_12` (
+CREATE TABLE `setfile` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `serial_number` int NOT NULL,
-  `Tunning_param` varchar(512) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `mode_id` int NOT NULL,
+  `setting_id` int NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `full_name` varchar(512) NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `selectedmv` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `mode_id` (`mode_id`),
+  KEY `setting_id` (`setting_id`),
+  CONSTRAINT `setfile_ibfk_1` FOREIGN KEY (`mode_id`) REFERENCES `mode` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `setfile_ibfk_2` FOREIGN KEY (`setting_id`) REFERENCES `setting` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `jn16_prs_cphy_12`
+-- Dumping data for table `setfile`
 --
 
-LOCK TABLES `jn16_prs_cphy_12` WRITE;
-/*!40000 ALTER TABLE `jn16_prs_cphy_12` DISABLE KEYS */;
-INSERT INTO `jn16_prs_cphy_12` VALUES (1,1,'MCLK');
-INSERT INTO `jn16_prs_cphy_12` VALUES (2,2,'PHY_TYPE');
-INSERT INTO `jn16_prs_cphy_12` VALUES (3,3,'PHY_LANE');
-INSERT INTO `jn16_prs_cphy_12` VALUES (4,4,'MIPI_DATA_RATE');
-INSERT INTO `jn16_prs_cphy_12` VALUES (5,5,'FPS');
-INSERT INTO `jn16_prs_cphy_12` VALUES (6,6,'SCRAMBLE_EN');
-/*!40000 ALTER TABLE `jn16_prs_cphy_12` ENABLE KEYS */;
+LOCK TABLES `setfile` WRITE;
+/*!40000 ALTER TABLE `setfile` DISABLE KEYS */;
+INSERT INTO `setfile` VALUES (2,8,33,'A01','A2A2_A01.nset','2025-04-03 18:50:45','10,0');
+INSERT INTO `setfile` VALUES (3,7,33,'A02','FULL_A02.nset','2025-04-03 18:50:45','10,0');
+/*!40000 ALTER TABLE `setfile` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -54,4 +58,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-06-01  0:36:20
+-- Dump completed on 2025-06-01  0:36:19
